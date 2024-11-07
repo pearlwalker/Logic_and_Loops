24ed1c5b706f14346188de98bf4ffb885b280d53
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-07 15:25:30 -0500

    Added 5 (+true) Endings
    Added all endings plus easter eggs
    
    Added all the console messages to tell story

--- Program.cs
+++ Program.cs
@@ -21,107 +21,127 @@ namespace Logic_and_Loops
 
             //Grab some user input
             playerName = Console.ReadLine();
-            Console.WriteLine(playerName+ ", huh?...What a nice name :)\n");
+            Console.WriteLine(playerName + ", huh?...What a nice name :)\n");
 
             //Continue the story
             Console.WriteLine("" +
-                "You see a clearing up ahead\n" + 
+                "You see a clearing up ahead\n" +
                 "You walk forward and find yourself at the intersection of two paths: \n" +
-                "To your left, the setting sun illuminates a trail marker. Going west will certainly lead you to other people. But you don't know who awaits you.\n"+
+                "To your left, the setting sun illuminates a trail marker. Going west will certainly lead you to other people. But you don't know who awaits you.\n" +
                 "To your right, you see a cave. With night coming quickly, you can sleep in comfort and protection if only you go east. But you won't be closer to finding your way home.\n");
 
             //Ask player to choose path
             Console.WriteLine("Which direction will you go?");
-            Console.WriteLine("[Type your direction using lowercase letters]\n");
+            Console.WriteLine("[Type 'left' to go left, 'right' to go right, or 'again' to see your options again]\n");
 
             //Grab more user input
             userChoice = Console.ReadLine();
-            Console.WriteLine("You chose: " + userChoice);
+            InitialChoice();
+            void InitialChoice()
+            {
+                //Select our path
+                if (userChoice == "left")
+                {
+                    //Console.WriteLine("You chose: " + userChoice);
+                    Console.WriteLine("\nYou go left and find a small village. The strangers are kind to you. You don't feel alone anymore.\n");
+                    Console.WriteLine("Community - Ending 1/5");
+                }
+                else if (userChoice == "right")
+                {
+                    //Console.WriteLine("You chose: " + userChoice);
+                    Console.WriteLine("\nYou go right and prepare to spend the night in the cave. It's secure and secluded. No one will bother you here.\n");
+                    Console.WriteLine("Protection - Ending 2/5");
+                }
+                else if (userChoice == "again")
+                {
+                    ChooseAgain(RepeatOptions());
+                }
+                else
+                {
+                    //user typed in something else
+                    Console.WriteLine("You chose: " + userChoice + ", which is not a valid option");
+                    userChoice = PseudoRepeat();
+                    InitialChoice();
+                }
+            }
+            static string PseudoRepeat()
+            {
+                string choice = Console.ReadLine();
+                return choice;
+            }
+            /*
+             * If user picks 'again'
+             * RepeatOptions will call on ChooseAgain
+             */
+            static string RepeatOptions()
+            {
+                Console.WriteLine("" +
+                    "The setting sun illuminates a trail marker.\n" +
+                    "There's a cave is shrouded in darkness.\n");
 
-            //Select our path
-            if (userChoice == "left" || userChoice == "west")
-            {
-                //go left
-                if (userChoice == "west")
-                {
-                    navigator = true;
-                }
-            } else if (userChoice == "right" || userChoice == "east")
-            {
-                //go right
-                if (userChoice == "east")
-                {
-                    navigator = true;
-                }
-            } else if (userChoice == "down")
-            {
-                //try to dig down
-                Console.WriteLine("You try to go down, but Hell is just out of reach. Don't worry, you'll get there eventually!");
-            } else if (userChoice == "back" || userChoice == "south")
-            {
-                //start program over? or say they go then come back
-            } else if (userChoice == "up")
-            {
-                //you hop but land on the same ground
-                Console.WriteLine("You try to go up, but Heaven is a little higher than your vertical.");
-            } else if (userChoice == "forward" || userChoice == "north")
-            {
-                //true ending, you go forward and find your name that they took from you
-            } else
-            {
-                //user typed in something else
+                Console.WriteLine("[But are those your only options?]\n");
+
+                //Returns "userChoice" value with local var
+                string repeatedChoice = Console.ReadLine();
+                return repeatedChoice;
             }
 
-        static void PathChoice(string choice, bool nav)
+            void ChooseAgain(string pick)
             {
-                            //Continue the story
-            Console.WriteLine("" +
-                "You see a clearing up ahead\n" + 
-                "You walk forward and find yourself at the intersection of two paths: \n" +
-                "To your left, the setting sun illuminates a trail marker. Going west will certainly lead you to other people. But you don't know who awaits you.\n"+
-                "To your right, you see a cave. With night coming quickly, you can sleep in comfort and protection if only you go east. But you won't be closer to finding your way home.\n");
-
-            //Ask player to choose path
-            Console.WriteLine("Which direction will you go?");
-            Console.WriteLine("[Type your direction using lowercase letters]\n");
-
-            //Grab more user input
-            choice = Console.ReadLine();
-            Console.WriteLine("You chose: " + choice);
-
-            //Select our path
-            if (choice == "left" || choice == "west")
-            {
-                //go left
-                if (choice == "west")
+                userChoice = pick;
+                //Select our path
+                if (userChoice == "left")
                 {
-                    nav = true;
+                    Console.WriteLine("\nYou follow the trail to completion, there are no more signs.\nBut the writing is pristine, these must have been put here recently.\nA person put this here, no doubt about it.\nAnd yet you're still alone.");
+                    Console.WriteLine("\nIsolation - Ending 3/5");
+
                 }
-            } else if (choice == "right" || choice == "east")
-            {
-                //go right
-                if (choice == "east")
+                else if (userChoice == "right")
                 {
-                    nav = true;
+                    Console.WriteLine("\nYou walk into the cave, only to find the darkness give way.\nYour eyes hurt from artificial LED light.\nYou thought you were safe from it, you should have been safe from it.\nBut it's too late.\nNow you'll never be alone.");
+                    Console.WriteLine("\nExposure - Ending 4/5");
                 }
-            } else if (choice == "down")
-            {
-                //try to dig down
-                Console.WriteLine("You try to go down, but Hell is just out of reach. Don't worry, you'll get there eventually!");
-            } else if (choice == "back" || choice == "south")
-            {
-                //start program over? or say they go then come back
-            } else if (choice == "up")
-            {
-                //you hop but land on the same ground
-                Console.WriteLine("You try to go up, but Heaven is a little higher than your vertical.");
-            } else if (choice == "forward" || choice == "north")
-            {
-                //true ending, you go forward and find your name that they took from you
-            } else
-            {
-                //user typed in something else
-            }
+                else if (userChoice == "down")
+                {
+                    Console.WriteLine("You try to go down, but Hell is just out of reach.\n");
+                    ChooseAgain(RepeatOptions());
+                }
+                else if (userChoice == "back")
+                {
+                    Console.WriteLine("It's not yours anymore.");
+                }
+                else if (userChoice == "up")
+                {
+                    Console.WriteLine("You try to go up, but Heaven is a little higher than your vertical.\n");
+                    ChooseAgain(RepeatOptions());
+                }
+                else if (userChoice == "forward")
+                {
+                    //true ending, you go forward and find your name that they took from you
+                    //Console.WriteLine("You chose: " + userChoice);
+                    Console.WriteLine("There's a lake in front of you.\nYou don't understand how you didn't notice it before.\nWhen you look in the water, you see someone.\nYou call out their name:");
+                    userChoice = Console.ReadLine();
+                    if (userChoice == playerName)
+                    {
+                        Console.WriteLine("\nAnd " + playerName + " smiles at their reflection.");
+                        Console.WriteLine("\nIdentity - True Ending");
+                    } else
+                    {
+                        Console.WriteLine("\nBut your voice is drowned by the water.");
+                        Console.WriteLine("\nReflection - Ending 5/5");
+                    }
+                    
+                }
+                else if (userChoice == "again")
+                {
+                    ChooseAgain(RepeatOptions());
+                }
+                else
+                {
+                    //user typed in something else
+                    Console.WriteLine("You chose: " + userChoice + ", which is not a valid option\n");
+                    ChooseAgain(RepeatOptions());
+                }
             }
         }
     }
