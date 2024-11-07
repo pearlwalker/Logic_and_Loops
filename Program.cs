c5180704c78d839d35a53858ba99d1aded5fdd43
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-06 20:49:20 -0500

    playerName init + Start program
    Declared and initialized playerName string var
    
    Began console messages, from introduction to the name request

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace Logic_and_Loops
         static void Main(string[] args)
         {
             Console.WriteLine("Logic and Loops!\n");
+
+            //Variable declaration
+            string playerName = "adventurer"; //init default value
+
+            //Start program
+            Console.Write("Here at 'Logic and Loops', we have some fun\nWe like to print stuff to the console\nWe like to explore\nAnd we like knowing names!\n\n");
+            Console.WriteLine("But it seems we don't know your name...");
+            Console.WriteLine("Can you give us your name?");
+            Console.WriteLine("[Enter your name, then press 'ENTER' on the keyboard]");
         }
     }
 }
