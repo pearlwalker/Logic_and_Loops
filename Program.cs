48a59f1092aac856079585f9ddfab47c938ec5f0
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-06 20:40:55 -0500

    Create Program.cs
    Created Program.cs with project template.
    
    "Logic and Loops!" is printed to console.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logic_and_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logic and Loops!\n");
+        }
+    }
+}
